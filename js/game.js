bef0cbb9acb64ea69bc1955f37452f4615b3c079
joshgeransky <jgeransky91@gmail.com>
2019-04-30 21:53:47 -0700

    added separate js file for the game, currently non-functional

---
+++ js/game.js
@@ -0,0 +1,25 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload () {
+    this.load.image('grass', '../images/grassSprite.png');
+}
+
+function create () {
+    this.add.image(400, 300, 'grass');
+}
+
+function update () {
+    
+}
+    
