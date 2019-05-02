f0eb47e37ce2644dc27439721543b5cb6e8e0a64
joshgeransky <jgeransky91@gmail.com>
2019-05-02 15:20:04 -0700

    Random tree generation, and game title added

--- js/main.js
+++ js/main.js
@@ -16,13 +16,27 @@ var config = {
     function preload () {
         this.load.image('background', '../assets/splash/800x600-grass-background.png');
         this.load.image('logo', '../assets/splash/title-text.png');
+        this.load.image('tree1', '../assets/sprites/tree1.png');
     }
 
     function create () {
         
-        this.add.image(400, 300, 'background');
-        scoreText = this.add.text(15, 100, 'Save the Forest', { fontSize: '128px', fill: 'white', fontFamily: 'VT323' });
-
-
+        background = this.add.image(400, 300, 'background');
+                
+        for (let i = 0; i < 1000; i++) {
+            var randX = Math.floor(Math.random() * 800);
+            var randY = Math.floor(Math.random() * 600);
+            
+            tree = this.add.image(randX, randY, 'tree1');
+        }
+        
+        titleText = this.add.text(15, 100, 'Save the Forest', { fontSize: '128px', fill: 'white', fontFamily: 'VT323' });
+        playButton = this.add.text(300, 300, 'PLAY', {
+            fontSize: '100pt',
+            fill: 'white',
+            fontFamily: 'VT323',
+            backgroundColor: 'blue',
+            
+        });
         
     }
