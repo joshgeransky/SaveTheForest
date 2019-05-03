f9a1f863d4596655f72aa8f203fa0cc6b44a260a
joshgeransky <47646774+joshgeransky@users.noreply.github.com>
2019-05-03 07:34:35 -0700

    Fixing game.js conflicts.
    Also adding random tree generation.

--- js/game.js
+++ js/game.js
@@ -1,36 +1,51 @@
+//Variable holding configuration
 var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        parent: 'game',
+        
+        scene: {
+            preload: preload,
+            create: create
+        }
+    };
+
+    //Create the game variable
+    var game = new Phaser.Game(config);
+    
+    //Preloading function
+    function preload () {
+        this.load.image('background', '../assets/splash/800x600-grass-background.png');
+        this.load.image('logo', '../assets/splash/title-text.png');
+        this.load.image('tree1', '../assets/sprites/tree1.png');
     }
-};
 
-var game = new Phaser.Game(config);
-let numOfTrees = Math.floor(Math.Random() * 49 + 1);
-console.log(numOfTrees);
-function preload () {
-    this.load.image('grass', '../assets/sprites/grassSprite1.png');
-	this.load.image('tree','../assets/sprites/Tree_1.png');
-}
-
-function create () {
-    this.add.image(400, 300, 'grass');
-	
-	this.add.image(300,300,'tree');
-	this.add.image(500,200,'tree');
-	this.add.image(600,375,'tree');
-	this.add.image(350,490,'tree');
-	this.add.image(500, 400,'tree');
-	this.add.image(290,120,'tree');
-	this.add.image(200,200,'tree');
-	
-}
-
-function update () {
-    
-}
-    
+    //Creation function
+    function create () {
+        
+        //Add the preset grass background (800x600)
+        background = this.add.image(400, 300, 'background');
+                
+        //For loop to randomly generate trees around the map
+        for (let i = 0; i < 1000; i++) {
+            var randX = Math.floor(Math.random() * 800);
+            var randY = Math.floor(Math.random() * 600);
+            
+            tree = this.add.image(randX, randY, 'tree1');
+        }
+        
+        //Create title text
+        this.titleText = this.add.text(15, 100, 'Save the Forest', { fontSize: '128px', fill: 'white', fontFamily: 'VT323' });
+        
+        //Create play button text --- Not Functional, should be actual button
+        this.playButton = this.add.text(300, 300, 'PLAY', {
+            fontSize: '100pt',
+            fill: 'white',
+            fontFamily: 'VT323',
+            backgroundColor: 'blue',
+            
+        });
+        
+        
+    }
