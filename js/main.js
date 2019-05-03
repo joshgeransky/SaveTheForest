bb5018b46d386adbfc123972b82d080f96541655
debby <debbychiu@outlook.com>
2019-05-03 12:48:31 -0700

    added start game button and functionality so the game can be started

--- js/main.js
+++ js/main.js
@@ -21,20 +21,30 @@ var config = {
 	var value = Phaser.Math.Between(4, 10);
 	var i = 0;
 	var hsv = [];
+	var startBtn;
+	 var titleText;
+	 var subText
+	  var length = Math.floor(Math.random() * 200) + 100;
+	  var tree;
+	   var treeArr = [length];
+	              
     //Preloading function
     function preload () {
         this.load.image('background', '../assets/splash/800x600-grass-background.png');
         this.load.image('logo', '../assets/splash/title-text.png');
         this.load.image('tree1', '../assets/sprites/tree1(64x64).png');
         this.load.image('tiles', 'assets/sprites/grassTile2.png');
+        this.load.image('startBtn', '../assets/sprites/startBtn.png');
     }
 
+	function start() {
+	console.log("Hello");
+	}
     //Creation function
     function create () {
         
         //Add the preset grass background (800x600)
         //background = this.add.image(400, 300, 'background');
-        
         
         //Add tiled background
          var level = [];
@@ -55,27 +65,51 @@ var config = {
 			
 		    this.cameras.main.setBounds(0, 0, layer.width, layer.height);
 	                
+	               
+	               
 	        //For loop to randomly generate trees around the map
-	        for (let i = 0; i < (Math.floor(Math.random() * 200) + 100); i++) {
+	        for (let i = 0; i < length; i++) {
 	            var randX = Math.floor(Math.random() * 800);
 	            var randY = Math.floor(Math.random() * 600);
-	            
 	            tree = this.add.image(randX, randY, 'tree1');
+	            treeArr[i] = tree;
 	        }
         
         //Create title text
-        this.titleText = this.add.text(15, 100, 'Save the Forest', { fontSize: '128px', fill: 'white', fontFamily: 'VT323' });
+        titleText = this.add.text(15, 100, 'Save the Forest', { fontSize: '128px', fill: 'white', fontFamily: 'VT323' });
         
         //Create subtext
-        this.subText = this.add.text(200, 200, 'Tap the fires to save the forest!', { fontSize: '24pt', fill: 'white', fontFamily: 'VT323'});
-        
+        subText = this.add.text(200, 200, 'Tap the fires to save the forest!', { fontSize: '24pt', fill: 'white', fontFamily: 'VT323'});
+     
+      /**  
         //Create play button text --- Not Functional, should be actual button
-        this.playButton = this.add.text(300, 300, 'PLAY', {
+        let playButton = this.add.text(300, 300, 'PLAY', {
             fontSize: '100pt',
             fill: 'white',
             fontFamily: 'VT323',
             backgroundColor: 'blue',
             
         });
-
+        */
+        
+        startBtn = this.add.sprite(420, 400, 'startBtn').setInteractive();
+        
+        startBtn.on('pointerdown', startGame);
     }
+    
+
+function startGame() {
+   titleText.visible = false;
+   subText.visible = false;
+   startBtn.visible = false;
+	  
+	  //Removing all trees and planting new ones to "start game"
+	  for (let i = 0; i < length; i++){
+	  	destroySprite(treeArr[i]);
+	  }
+}
+
+//should destroy the sprites
+function destroySprite(sprite) {
+	sprite.destroy();
+}
