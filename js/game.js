73769dd1fb1c11bd7dc2723e562addcc67ffeddb
Edmond Wong <wongedmondk@gmail.com>
2019-05-03 02:39:31 -0700

    Implemented flame sprite

--- js/game.js
+++ js/game.js
@@ -15,6 +15,8 @@ console.log(numOfTrees);
 function preload () {
     this.load.image('grass', '../assets/sprites/grassSprite1.png');
 	this.load.image('tree','../assets/sprites/Tree_1.png');
+  
+    this.load.image('flame', '../assets/sprites/Flame.png');
 }
 
 function create () {
@@ -27,6 +29,15 @@ function create () {
 	this.add.image(500, 400,'tree');
 	this.add.image(290,120,'tree');
 	this.add.image(200,200,'tree');
+  
+    this.add.image(300, 300, 'flame');
+	this.add.image(500,200,'flame');
+	this.add.image(600,375,'flame');
+	this.add.image(350,490,'flame');
+	this.add.image(500, 400,'flame');
+	this.add.image(290,120,'flame');
+	this.add.image(200,200,'flame');
+    
 	
 }
 
