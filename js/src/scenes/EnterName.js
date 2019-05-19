8ccdd3f6eaecd14f5d51e9a4dc2632630d46179a
Justin Tendeck <justin.tendeck@gmail.com>
2019-05-19 12:39:05 -0700

    modified high score logic

--- js/src/scenes/EnterName.js
+++ js/src/scenes/EnterName.js
@@ -16,7 +16,7 @@ class EnterName extends Phaser.Scene {
     
     create ()
     {
-        this.add.text(300, 310, playerScore, {fontSize: '60pt', fontFamily: 'VT323', fill: 'white'});
+        
 
         var chars = [
             [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' ],
@@ -29,101 +29,22 @@ class EnterName extends Phaser.Scene {
         var input = this.add.bitmapText(130, 50, 'arcade', 'ABCDEFGHIJ\n\nKLMNOPQRST\n\nUVWXYZ.-').setLetterSpacing(20);
     
         input.setInteractive();
-    
+
         var rub = this.add.image(input.x + 430, input.y + 148, 'rub');
         var end = this.add.image(input.x + 482, input.y + 148, 'end');
     
         var block = this.add.image(input.x - 10, input.y - 2, 'block').setOrigin(0);
     
         var legend = this.add.bitmapText(80, 260, 'arcade', 'RANK  SCORE   NAME').setTint(0xff00ff);
+
+        var scoreTex = this.add.bitmapText(300, 310, 'arcade', playerScore).setTint(0xffffff);
     
         var playerText = this.add.bitmapText(560, 310, 'arcade', name).setTint(0xff0000);
-    
-        this.input.keyboard.on('keyup', function (event) {
-    
-            if (event.keyCode === 37)
-            {
-                //  left
-                if (cursor.x > 0)
-                {
-                    cursor.x--;
-                    block.x -= 52;
-                }
-            }
-            else if (event.keyCode === 39)
-            {
-                //  right
-                if (cursor.x < 9)
-                {
-                    cursor.x++;
-                    block.x += 52;
-                }
-            }
-            else if (event.keyCode === 38)
-            {
-                //  up
-                if (cursor.y > 0)
-                {
-                    cursor.y--;
-                    block.y -= 64;
-                }
-            }
-            else if (event.keyCode === 40)
-            {
-                //  down
-                if (cursor.y < 2)
-                {
-                    cursor.y++;
-                    block.y += 64;
-                }
-            }
-            else if (event.keyCode === 13 || event.keyCode === 32)
-            {
-                //  Enter or Space
-                if (cursor.x === 9 && cursor.y === 2 && name.length > 0)
-                {
-                    //  Submit
-
-                    var saveData = {
-                        name: name,
-                        metrics: {
-                        count: playerScore * -1
-                        }
-                    }
-
-                    $('#scoreTable').empty()
-        
-                    ref.push(saveData);
-
-                    this.scene.start('TitleScene');
-                
-                }
-                else if (cursor.x === 8 && cursor.y === 2 && name.length > 0)
-                {
-                    //  Rub
-                    name = name.substr(0, name.length - 1);
-    
-                    playerText.text = name;
-                }
-                else if (name.length < 3)
-                {
-                    //  Add
-                    name = name.concat(chars[cursor.y][cursor.x]);
-    
-                    playerText.text = name;
-                }
-            }
-    
-        });
     
         input.on('pointermove', function (pointer, x, y) {
     
             var cx = Phaser.Math.Snap.Floor(x, 52, 0, true);
             var cy = Phaser.Math.Snap.Floor(y, 64, 0, true);
-
-            if (cy > 2) {
-                cy = 2;
-            }
             
             var char = chars[cy][cx];
     
@@ -139,10 +60,6 @@ class EnterName extends Phaser.Scene {
     
             var cx = Phaser.Math.Snap.Floor(x, 52, 0, true);
             var cy = Phaser.Math.Snap.Floor(y, 64, 0, true);
-
-            if (cy > 2) {
-                cy = 2;
-            }
 
             var char = chars[cy][cx];
     
@@ -177,15 +94,21 @@ class EnterName extends Phaser.Scene {
                 this.scene.start('TitleScene');
 
             }
+
             else if (name.length < 3)
             {
                 //  Add
+                if (char !== '>') {
                 name = name.concat(char);
-    
+
                 playerText.text = name;
+
+                } else {
+                    this.cameras.main.shake(475, 0.013, 0.4);
+                }
+                
             }
     
         }, this);
-    
     }
 }
